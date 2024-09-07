af287b791eb09b451360b82d2b105a8956e9ba21
i am dk <iamdk353@gmail.com>
2024-09-07 06:07:51 +0530

    sheet trigger bug fixed

--- src/components/ui/MobileSignin.tsx
+++ src/components/ui/MobileSignin.tsx
@@ -8,6 +8,7 @@ import {
   // SheetDescription,
   // SheetFooter,
   SheetHeader,
+  SheetOverlay,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
@@ -16,40 +17,45 @@ import { Menu } from "lucide-react";
 import SignIn from "../signIn";
 import Logout from "../Logout";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 const MobileSignin = () => {
   const { isVerified } = useUserStore();
+  const [isOpen, setOpen] = useState(true);
   const navigate = useNavigate();
   return (
     <div className="md:hidden">
-      <Sheet>
+      <Sheet open={isOpen} onOpenChange={setOpen}>
         <SheetTrigger asChild>
-          <Button variant="outline">
+          <Button variant="outline" onClick={() => setOpen(!open)}>
             <Menu />
           </Button>
         </SheetTrigger>
-        <SheetContent className="space-y-5">
-          <SheetHeader>
-            {!isVerified && <SheetTitle>Sign in to get started</SheetTitle>}
-            {isVerified && (
-              <SheetTitle className="text-left">HELLO {}</SheetTitle>
+        <SheetOverlay>
+          <SheetContent className="space-y-5">
+            <SheetHeader>
+              {!isVerified && <SheetTitle>Sign in to get started</SheetTitle>}
+              {isVerified && (
+                <SheetTitle className="text-left">HELLO {}</SheetTitle>
+              )}
+            </SheetHeader>
+            {isVerified ? (
+              <>
+                <Button
+                  className="w-full"
+                  onClick={() => {
+                    setOpen(!open);
+                    navigate("profile");
+                  }}
+                >
+                  PROFILE
+                </Button>
+                <Logout />
+              </>
+            ) : (
+              <SignIn />
             )}
-          </SheetHeader>
-          {isVerified ? (
-            <>
-              <Button
-                className="w-full"
-                onClick={() => {
-                  navigate("profile");
-                }}
-              >
-                PROFILE
-              </Button>
-              <Logout />
-            </>
-          ) : (
-            <SignIn />
-          )}
-        </SheetContent>
+          </SheetContent>
+        </SheetOverlay>
       </Sheet>
     </div>
   );
